ffb423397b26de70aee6fce17790b014cd0ba21e
langung102 <minhlangwalker@gmail.com>
2023-09-30 21:40:20 +0700

    first commit

---
+++ main/periodic_print.c
@@ -0,0 +1,69 @@
+// /*
+//  * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+//  *
+//  * SPDX-License-Identifier: CC0-1.0
+//  */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
+
+#define GPIO_BUTTON_PIN         4
+#define GPIO_LED_PIN            2
+#define ESP_INTR_FLAG_DEFAULT   0
+
+static QueueHandle_t gpio_evt_queue = NULL;
+
+void print_id(void* arg) {
+    while(1) {
+        printf("2011507\n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    } 
+}
+
+static void IRAM_ATTR gpio_isr_handler(void* arg)
+{
+    uint32_t gpio_num = (uint32_t) arg;
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
+
+static void print_eps32(void* arg)
+{
+    uint32_t io_num;
+    while(1) {
+        if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+            printf("ESP32\n");
+        }
+    }
+}
+
+void app_main(void)
+{
+    gpio_config_t io_conf = {};
+    // io_conf.intr_type = GPIO_INTR_DISABLE;
+    // io_conf.mode = GPIO_MODE_OUTPUT;
+    // io_conf.pin_bit_mask = GPIO_LED_PIN;
+    // io_conf.pull_down_en = 0;
+    // io_conf.pull_up_en = 0;
+    // gpio_config(&io_conf);
+
+    io_conf.intr_type = GPIO_INTR_ANYEDGE;
+    io_conf.pin_bit_mask = GPIO_BUTTON_PIN;
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.pull_up_en = 1;
+    gpio_config(&io_conf);
+
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+    
+    // xTaskCreate(&print_id, "print_id", 2048, NULL, 5, NULL);
+    xTaskCreate(print_eps32, "print_eps32", 1024, NULL, 5, NULL);
+
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    gpio_isr_handler_add(GPIO_BUTTON_PIN, gpio_isr_handler, (void*) GPIO_BUTTON_PIN);
+}
