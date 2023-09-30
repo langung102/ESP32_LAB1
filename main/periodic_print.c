1e2db3ae6036c6680787f00a5ba354c4941935fd
langung102 <minhlangwalker@gmail.com>
2023-09-30 22:32:04 +0700

    add led

--- main/periodic_print.c
+++ main/periodic_print.c
@@ -38,6 +38,7 @@ static void print_eps32(void* arg)
     uint32_t io_num;
     while(1) {
         if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+            gpio_set_level(BLINK_GPIO, gpio_get_level(io_num));
             printf("ESP32\n");
         }
     }
@@ -46,22 +47,22 @@ static void print_eps32(void* arg)
 void app_main(void)
 {
     gpio_config_t io_conf = {};
-    // io_conf.intr_type = GPIO_INTR_DISABLE;
-    // io_conf.mode = GPIO_MODE_OUTPUT;
-    // io_conf.pin_bit_mask = GPIO_LED_PIN;
-    // io_conf.pull_down_en = 0;
-    // io_conf.pull_up_en = 0;
-    // gpio_config(&io_conf);
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pin_bit_mask = 1ULL<<GPIO_LED_PIN;
+    io_conf.pull_down_en = 0;
+    io_conf.pull_up_en = 0;
+    gpio_config(&io_conf);
 
-    io_conf.intr_type = GPIO_INTR_ANYEDGE;
-    io_conf.pin_bit_mask = GPIO_BUTTON_PIN;
+    io_conf.intr_type = GPIO_INTR_POSEDGE;
+    io_conf.pin_bit_mask = 1ULL<<GPIO_BUTTON_PIN;
     io_conf.mode = GPIO_MODE_INPUT;
     io_conf.pull_up_en = 1;
     gpio_config(&io_conf);
 
     gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
     
-    // xTaskCreate(&print_id, "print_id", 2048, NULL, 5, NULL);
+    xTaskCreate(&print_id, "print_id", 2048, NULL, 5, NULL);
     xTaskCreate(print_eps32, "print_eps32", 1024, NULL, 5, NULL);
 
     gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
